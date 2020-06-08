6e34fcb381fd3ede704bfeff64a13b6855216042
Douglas Arigah <56113272+darigah@users.noreply.github.com>
2020-06-08 13:30:16 +0300

    tidying up
    tidying up

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 let CC, YY, MM, DD, d, dayValue;
 let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 function validate() {
   var genders = document.getElementsByName("gender");
